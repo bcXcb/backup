fb0d2af24ef64537f766e038b9c4a1a28c18b482
bcXcb <gabriel.lcifba@gmail.com>
2023-05-09 12:56:28 -0300

    Modularização

--- bak.sh
+++ bak.sh
@@ -5,37 +5,63 @@ declare -r TEMP_DIR=/home/gabriel/$((RANDOM % 256)) # 0 to 255
 declare -r MOUNTAGE_POINT=/media/gabriel
 declare -r DEVICE=/dev/sdb1
 declare -r DESTINY=$MOUNTAGE_POINT/backup
-declare -r INPUT=/home/gabriel/files/projetos/github/bak/input.txt
 declare -r BACKUP_FILE=backup-`date +"%a"`.zip
-
-declare -a items
 declare -a empty_items
 declare -a non_empty_items
 
-declare -i i=0
-declare -i j=0
 
-for item in `cat $INPUT`; do
-	items[$((i++))]=$item
-done
+fill_arrays() {
+    declare -i local i
+    declare -i local j
+    declare -a local items
+    declare -r local ITEMS=/home/gabriel/files/projetos/github/bak/items.txt
+    
+	i=0
+	for item in `cat $ITEMS`; do
+		items[$((i++))]=$item
+	done
 
-i=0
-for item in ${items[*]}; do
-	if [ -d $item ]; then
-		[[ `ls $item | wc -l` -eq 0 ]] && empty_items[$i]=$item || non_empty_items[$j]=$item
-	else
-        [[ -s $item ]] && non_empty_items[$j]=$item || empty_items[$i]=$item
-	fi
-	let i++ j++
-done
+	i=0
+	j=0
+	for item in ${items[*]}; do
+		if [ -e $item ]; then
+			[[ `ls $item | wc -l` -eq 0 ]] && empty_items[$i]=$item || non_empty_items[$j]=$item
+		else
+        	[[ -s $item ]] && non_empty_items[$j]=$item || empty_items[$i]=$item
+		fi
+		((i++))
+		((j++))
+	done
+}
+
+backup() {
+    if [ -n "$non_empty_items" ]; then
+        echo "The following items was copied:"
+        for item in ${non_empty_items[*]}; do
+            cp -rp $item $TEMP_DIR
+            if [ -d $item ]; then
+                echo "(+) $item. (dir)"
+            else
+                echo "(+) $item. (file)"
+            fi
+        done
+    fi
+
+    if [ -z "$empty_items" ]; then
+        echo "The following items was ignored:"
+        for item in ${empty_items[*]}; do
+            if [ -d $item ]; then
+                echo "(-) $item. (dir) (empty)"
+            else
+                echo "(-) $item. (file) (empty)"
+            fi
+        done
+    fi
+}
 
 clear
 
-echo 'x--------------------------------x'
-echo '| BAK: BACKUP IN USB FLASH DRIVE |'
-echo 'x--------------------------------x'
-
-echo -en "\n"
+fill_arrays
 
 echo "Starting backup..."
 
@@ -50,20 +76,7 @@ if [ ! -d $DESTINY ]; then
     echo "Was created the directory \"$DESTINY\"."
 fi
 
-if [ -n "$non_empty_items" ]; then
-	echo "The following items was copied:"
-	for item in ${non_empty_items[*]}; do
-		cp -rp $item $TEMP_DIR
-		echo "+ $item."
-	done
-fi
-
-if [ -z "$empty_items" ]; then
-	echo "The following items was ignored:"
-	for item in ${empty_items[*]}; do
-    	echo "- $item."
-	done
-fi
+backup
 
 echo "Compressing items..."
 zip -rq9 $BACKUP_FILE $TEMP_DIR
