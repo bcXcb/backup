f1e69e8c663b49b3da25d7e6ef16dfb283d181fc
bcXcb <gabriel.lcifba@gmail.com>
2023-05-19 14:52:36 -0300

    Verificação se pendrive está ocupado

--- bak.sh
+++ bak.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 
-# Backup in flash drives
+# Backup of directories and files in flash drives
 # The script works fine in Debian Linux
 
 # To improve
-# - the mount point must be a command line parameter
-# - to create the function "check_dependences" for check and fix (if possible) the dependence problems
+# - to create the function "check_dependences" for check and fix (if possible) the dependence problems or stop the execution
 
-declare MOUNT_POINT=/media/gabriel
-declare DESTINY=$MOUNT_POINT/backup
+declare flash_drive_path=
+declare MOUNT_POINT_PATH=/media/gabriel
+declare BACKUP_DIR_PATH=$MOUNT_POINT_PATH/backup
 declare TEMP_DIR_NAME=$((RANDOM % 256)) # 0 to 255, an octect
-declare TEMP_DIR=$DESTINY/$TEMP_DIR_NAME
+declare TEMP_DIR_PATH=$BACKUP_DIR_PATH/$TEMP_DIR_NAME
 declare BACKUP_FILE_NAME=`date +"%A"`.zip
-declare BACKUP_FILE_PATH=$DESTINY/$BACKUP_FILE_NAME
-declare device
+declare BACKUP_FILE_PATH=$BACKUP_DIR_PATH/$BACKUP_FILE_NAME
 
 function help {
 	echo 'NAME'
@@ -53,8 +52,8 @@ function parameters {
             version
             exit 0;;
         '-d' | '--device')
-            if [ -n "$2" ]; then
-                device=$2
+            if [ -n "$2" -a -e $2 ]; then
+                flash_drive_path=$2
             else
                 help
                 exit 1
@@ -66,42 +65,42 @@ function parameters {
 }
 
 
-# review the implementation
+# review this implementation
 function toggle_flash_drive_mount {
 	local action=$1
 
 	if [ $action = '--mount' ]; then
-    	mountpoint -q $MOUNT_POINT
+    	mountpoint -q $MOUNT_POINT_PATH
 
 	    if [ $? -eq 0 ]; then
     	    echo 'The flash drive already mounted.'
 	    else
         	echo -n 'Mounting flash drive...'
-    	    mount $device $MOUNT_POINT 2> /dev/null && echo ' [Success].' || echo ' [ Failure].'
+    	    mount $flash_drive_path $MOUNT_POINT_PATH 2> /dev/null && echo ' [Success].' || echo ' [ Failure].'
 	    fi
 	else
-    	mountpoint -q $MOUNT_POINT
+    	mountpoint -q $MOUNT_POINT_PATH
 
 	    if [ $? -ne 0 ]; then
     	    echo 'The flash drive already dismounted.'
 	    else
         	echo -n 'Dismounting flash drive...'
-    	    umount $device 2> /dev/null && echo ' [Success].' || echo ' [Failure].'
+    	    umount $flash_drive_path 2> /dev/null && echo ' [Success].' || echo ' [Failure].'
 	    fi
 	fi
 }
 
 function create_dirs {
-    if [ ! -e $DESTINY ]; then
-        mkdir -p $DESTINY && echo "Was created the directory \"$DESTINY\"."
+    if [ ! -e $BACKUP_DIR_PATH ]; then
+        mkdir -p $BACKUP_DIR_PATH && echo "Was created the directory \"$BACKUP_DIR_PATH\"."
     fi
 
-    mkdir -p $TEMP_DIR && echo "Was created the temporary directory \"$TEMP_DIR_NAME\"."
+    mkdir -p $TEMP_DIR_PATH && echo "Was created the temporary directory \"$TEMP_DIR_NAME\"."
 }
 
 function backup {
     local i=0
-    local items
+    local items=
 	local file_name='dirs-and-files.txt'
     local ITEMS_LIST=/home/gabriel/files/projetos/github/bak/$file_name
 
@@ -120,7 +119,7 @@ function backup {
 	echo 'd - directory, f - file.'
     echo 'The following items were copied:'
     for item in ${items[*]}; do
-        cp -rp $item $TEMP_DIR
+        cp -rp $item $TEMP_DIR_PATH
         [[ -d $item ]] && echo "(d) $item." || echo "(f) $item."
     done
 
@@ -133,7 +132,7 @@ function compress {
     fi
 
     echo -n 'Compressing items...'
-    zip -rq0 $BACKUP_FILE_PATH $TEMP_DIR 2> /dev/null && echo ' [Success].' || echo ' [Failure].'
+    zip -rq0 $BACKUP_FILE_PATH $TEMP_DIR_PATH 2> /dev/null && echo ' [Success].' || echo ' [Failure].'
 }
 
 function defragment {
@@ -142,7 +141,13 @@ function defragment {
 }
 
 function clean {
-    rm -r $TEMP_DIR && echo 'Temporary directory was removed.'
+    rm -r $TEMP_DIR_PATH && echo 'Temporary directory was removed.'
+}
+
+function flash_drive_is_busy {
+	fuser -s $flash_drive_path
+
+	[[ $? -eq 0 ]] && echo 'True' || echo 'False'
 }
 
 function sound_device_is_busy {
@@ -154,7 +159,7 @@ function sound_device_is_busy {
 }
 
 function notification {
-	local is_busy
+	local is_busy=
 	local file_name='sound.wav'
 	local sound=/home/gabriel/files/projetos/github/bak/$file_name
 
@@ -166,6 +171,13 @@ function notification {
 }
 
 parameters $*
+if [ `flash_drive_is_busy` = 'True' ]; then
+    echo 'The flash drive is busy.'
+    echo -n 'Waiting...'
+    while [ `flash_drive_is_busy` = 'True' ]; do
+        : # nothing
+    done
+fi
 echo 'Starting backup...'
 toggle_flash_drive_mount --mount
 create_dirs
